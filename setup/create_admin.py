be9588637a99fd26beed448ae4609752bc4c4c67
bcsanford <Brett.Sanford@gmail.com>
2025-07-16 00:50:20 -0400

    Update create_admin.py

--- setup/create_admin.py
+++ setup/create_admin.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))import os
 import getpass
 from app import create_app, db
 from app.models import User
